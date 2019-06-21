8be48e0336fa81b9fae3eec5b431bc919bbd0c67
ZW_Li <4a5g0030@stust.edu.tw>
2019-06-22 01:58:54 +0800

    2019_06_22 Median Blurring

--- line_follow.py
+++ line_follow.py
@@ -8,9 +8,13 @@ re, th = cv.threshold(gary, 0, 255, cv.THRESH_OTSU)
 Otsu = cv.cvtColor(th, cv.COLOR_BGR2RGB)
 plt.imshow(Otsu)
 h, w = Otsu.shape[:2]
-print(h,w)
 
 for i in np.arange(0, h, 100):
     cv.line(Otsu, (0, i), (w, i), (0, 0, 0), 2)
-
+for i in np.arange(0, w, 100):
+    cv.line(Otsu, (i, 0), (i, h), (0, 0, 0), 2)
 plt.imshow(Otsu)
+
+Otsu = cv.cvtColor(th, cv.COLOR_BGR2RGB)
+median = cv.medianBlur(Otsu, 55)
+plt.imshow(median)
