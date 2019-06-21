8c7a67733d22b6e01a44b22e34dd0177eec303f7
ZW_Li <4a5g0030@stust.edu.tw>
2019-06-21 10:29:42 +0800

    2019_06_21 Draw_line

--- line_follow.py
+++ line_follow.py
@@ -7,3 +7,10 @@ gary = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 re, th = cv.threshold(gary, 0, 255, cv.THRESH_OTSU)
 Otsu = cv.cvtColor(th, cv.COLOR_BGR2RGB)
 plt.imshow(Otsu)
+h, w = Otsu.shape[:2]
+print(h,w)
+
+for i in np.arange(0, h, 100):
+    cv.line(Otsu, (0, i), (w, i), (0, 0, 0), 2)
+
+plt.imshow(Otsu)
