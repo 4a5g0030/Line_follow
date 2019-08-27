d48f3709f668bc5eee4c110eb23139b3c4b00df4
ZW_Li <4a5g0030@stust.edu.tw>
2019-08-27 15:18:21 +0800

    create ShowProcess & conclusion

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import glob
 import follow_line as fl
 import time
 import numba as nb
+from ShowProcess import ShowProcess
 
 def func_time(func):
     def ft():
@@ -18,13 +19,15 @@ def main():
     img_in_root = 'test_data/image/'
     img_out_root = 'test_data/output/'
     img_count = len(glob.glob(img_in_root + '*jpg'))
+    process_bar = ShowProcess(img_count, 'OK!')
 
     for x in range(img_count):
         img = fl.planA(img_in_root + str(x).zfill(3) + '.jpg')
         img = img.windows()
-        # cv2.imwrite(img_out_root + str(x).zfill(3) + '.jpg', img)
-        print('{:.2%}'.format(x / img_count))
-    print('100%')
+        process_bar.show_process()
+    #     cv2.imwrite(img_out_root + str(x).zfill(3) + '.jpg', img)
+    #     print('{:.2%}'.format(x / img_count))
+    # print('100%')
 
 
 if __name__ == '__main__':
